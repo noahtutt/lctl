67c3a53f6a740883a948d1679b6ec61be6dfea1b
Noah Tutt <noahtutt@umich.edu>
2019-12-17 16:09:20 -0500

    switch to protected for member variables

--- pattern.hpp
+++ pattern.hpp
@@ -11,11 +11,13 @@ public:
 
 	explicit pattern(size_t);
 	
+	// ticks to the next iteration of updates
 	void next_iteration();	
 
+	// gets the rgb value at a given position for this iteration
 	virtual rgb get_value_at(size_t) = 0;	
 
-private:
+protected:
 
 	size_t iteration;
 	size_t size;
